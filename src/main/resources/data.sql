8cc8649ba22b5b15106a207ee1dd6dab404f4f7a
Karel Muñiz <oricha@gmail.com>
2017-11-10 11:46:29 +0100

    Added sql file with data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+
+insert into FILM(ID, NAME, PREMIUM_PRICE, BASIC_PRICE, type) values (x'01e75bc1f4674268bacde08ace6e714c','Matrix 11', '2.35', '2.00', 'PREMIUM' );
+insert into FILM(ID, NAME, PREMIUM_PRICE, BASIC_PRICE, type) values (x'bb4d5cb7679d429b908c6cf28eccb7ac','Spider Man', '2.12', '1.90', 'REGULAR' );
+insert into FILM(ID, NAME, PREMIUM_PRICE, BASIC_PRICE, type) values (x'6fbfb60988fe47229b9bf87f3eb7f2be','Spider Man 2', '2.12', '1.90', 'REGULAR' );
+
+
+
+
+
+insert into USER (ID, NAME, PASS, BONUS) values (x'85d8997b1f564a388679dd54d4d1ca31','Pepito', '12345', 0);
+
